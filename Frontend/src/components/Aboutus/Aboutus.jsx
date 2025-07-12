e286f1e3afc2ce4118fcdf8f0753cdf6c69c4809
Abhista-111005 <abhistachakraborty15@gmail.com>
2025-07-12 22:08:20 +0530

    1vh of the About US Page

--- Frontend/src/components/Aboutus/Aboutus.jsx
+++ Frontend/src/components/Aboutus/Aboutus.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../HomePage/Navbar'
+import AboutUs from './AboutUs1vh'
 
 const Aboutus = () => {
   return (
     <div>
       <Navbar/>
+      <AboutUs />
     </div>
   )
 }
