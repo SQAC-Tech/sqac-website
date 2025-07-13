0f525c58d01afeb2ef17dffb07fc9758f4c59c00
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-07-13 20:57:04 +0530

    retrieved importExcel data

--- backend/importExcel.js
+++ backend/importExcel.js
@@ -7,7 +7,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .then(() => console.log('MongoDB connected for import'))
   .catch(err => console.log(err));
 
-const workbook = xlsx.readFile('data.xlsx');
+const workbook = xlsx.readFile('Member Information Form For SQAC Website (Responses).xlsx');
 const sheetName = workbook.SheetNames[0];
 const worksheet = workbook.Sheets[sheetName];
 const jsonData = xlsx.utils.sheet_to_json(worksheet);
