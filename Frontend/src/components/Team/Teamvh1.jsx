c7b7eaa969c696b75991907fe39eea317c8b3046
Christin Joseph <christin.kjoseph@gmail.com>
2025-07-24 22:58:36 +0530

    responsiveness improved

--- Frontend/src/components/Team/Teamvh1.jsx
+++ Frontend/src/components/Team/Teamvh1.jsx
@@ -23,7 +23,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 1.7, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[53vh] left-[3vw] md:top-[53vh] bg-red-300 bg-opacity-40 text-lg font-bold font-['Stick_No_Bills'] rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-28 md:h-28 flex items-center justify-center"
+          className="absolute top-[53vh] left-[3vw] bg-red-300 bg-opacity-40 text-lg font-bold font-['Stick_No_Bills'] rounded-full w-27 h-27 flex items-center justify-center"
         >
           LEARNERS
         </motion.div>
@@ -31,7 +31,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 1.6, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[69vh] left-[8vw] sm:top-[70vh] md:top-[73vh] md:left-[17vh] bg-[#AFE7D4] bg-opacity-70 font-['Stick_No_Bills'] text-lg sm:text-lg font-bold rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-24 md:h-24 md:text-2xl flex items-center justify-center"
+          className="absolute top-[82vh] left-[8vw] md:top-[77vh] bg-[#AFE7D4] bg-opacity-70 font-['Stick_No_Bills'] text-2xl font-bold rounded-full w-24 h-24 flex items-center justify-center"
         >
           UI / UX
         </motion.div>
@@ -39,7 +39,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 2.2, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[49vh] left-[24vw] md:top-[55vh] w-28 h-28 sm:w-32 sm:h-32 md:w-40 md:h-40 rounded-full overflow-hidden border-2 border-gray-300"
+          className="absolute top-[58vh] left-[24vw]  w-40 h-40 rounded-full overflow-hidden border-2 border-gray-300"
         >
           <img src={teamImg2} alt="Puzzle Team" className="w-full h-full object-cover" />
         </motion.div>
@@ -47,7 +47,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 1.8, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[45vh] left-[44vw]  bg-[#C5FFAA] bg-opacity-55 font-['Stick_No_Bills'] text-lg sm:text-xl font-bold rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 flex items-center justify-center"
+          className="absolute top-[45vh] left-[36vw] md:left-[44vw] bg-[#C5FFAA] bg-opacity-55 font-['Stick_No_Bills'] text-lg sm:text-xl font-bold rounded-full w-32 h-32  flex items-center justify-center"
         >
           EVENTS
         </motion.div>
@@ -55,7 +55,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 1.9, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[65vh] left-[55vw] bg-[#FFE26F] bg-opacity-70 font-['Stick_No_Bills'] text-lg font-bold rounded-full w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 flex items-center justify-center"
+          className="absolute top-[79vh] left-[53vw] md:top-[72vh] md:left-[55vw]  bg-[#FFE26F] bg-opacity-70 font-['Stick_No_Bills'] text-lg font-bold rounded-full w-32 h-32  flex items-center justify-center"
         >
           Developers
         </motion.div>
@@ -63,7 +63,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 2.2, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[15vh] left-[80vw] md:left-[77vw] bg-[#97B6FF] bg-opacity-70 font-['Stick_No_Bills'] text-lg font-bold rounded-full w-24 h-24 sm:w-28 sm:h-28 flex items-center justify-center"
+          className="absolute top-[18vh] left-[70vw] md:left-[79vw]  bg-[#97B6FF] bg-opacity-70 font-['Stick_No_Bills'] text-lg font-bold rounded-full w-28 h-28  flex items-center justify-center"
         >
           MANAGERS
         </motion.div>
@@ -71,7 +71,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 2.4, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[56vh] left-[88vw] md:left-[83vw] bg-[#C5A9F4] bg-opacity-70 font-['Stick_No_Bills'] text-lg md:text-2xl font-bold rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-26 md:h-26 flex items-center justify-center"
+          className="absolute top-[66vh] left-[71vw] md:left-[82vw] bg-[#C5A9F4] bg-opacity-70 font-['Stick_No_Bills'] text-lg md:text-2xl font-bold rounded-full w-26 h-26  flex items-center justify-center"
         >
           TECHIE
         </motion.div>
@@ -79,7 +79,7 @@ const Teamvh1 = () => {
         <motion.div
           animate={{ y: [0, -25, 0] }}
           transition={{ duration: 2.3, repeat: Infinity, ease: 'easeInOut' }}
-          className="absolute top-[26vh] left-[60vw] md:top-[30vh] w-32 h-32 sm:w-36 sm:h-36 md:w-45 md:h-45 rounded-full overflow-hidden border-2 border-gray-300"
+          className="absolute top-[34vh] left-[60vw] md:left-[63vw] md:top-[38vh] w-45 h-45 rounded-full overflow-hidden border-2 border-gray-300"
         >
           <img src={teamImg1} alt="Team Meeting" className="w-full h-full object-cover" />
         </motion.div>
