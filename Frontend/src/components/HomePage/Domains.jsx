19c74b728e43e4128cbaffceae96263bdb5f530c
MJ-1112 <m22327803@gmail.com>
2025-07-28 12:31:12 +0530

    background of domains blended

--- Frontend/src/components/HomePage/Domains.jsx
+++ Frontend/src/components/HomePage/Domains.jsx
@@ -153,7 +153,7 @@ export default function Domains() {
   ];
 
   return (
-    <div className="py-20 lg:py-32 bg-gradient-to-b from-orange-300 via-fuchsia-100 to-white">
+    <div className="py-20 lg:py-32 bg-gradient-to-b from-orange-300 via-fuchsia-100 to-cyan-200">
       <h2 className="text-4xl lg:text-5xl font-bold mb-12 text-center text-[#3B0A4B]">
         Our Core Domains
       </h2>
