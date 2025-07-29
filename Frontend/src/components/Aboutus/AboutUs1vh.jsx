7b8efc3f5fc409192d7e05c5769266bc71c91e77
agrim-git-hub <as6358@srmist.edu.in>
2025-07-29 18:24:32 +0530

    fix - about us first VH

--- Frontend/src/components/Aboutus/AboutUs1vh.jsx
+++ Frontend/src/components/Aboutus/AboutUs1vh.jsx
@@ -15,8 +15,8 @@ const AboutUs = () => {
         <motion.h1
           initial={{ y: -40, opacity: 0 }}
           animate={isInView ? { y: 0, opacity: 1 } : {}}
-          transition={{ duration: 0.5, ease: "easeOut" }}
-          className="mt-9 text-5xl sm:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-orange-500 mb-6 font-poppins hover:scale-105 transition-transform"
+          transition={{ duration: 0.3, ease: "easeOut" }}
+          className="pt-[30px] mt-11 text-5xl sm:text-6xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-orange-500 mb-6 font-poppins hover:scale-105 transition-transform"
         >
           ABOUT US
         </motion.h1>
@@ -25,13 +25,13 @@ const AboutUs = () => {
           initial={{ scale: 0.97, opacity: 0 }}
           animate={isInView ? { scale: 1, opacity: 1 } : {}}
           transition={{ delay: 0.2, duration: 0.6, ease: "easeOut" }}
-          className="bg-gradient-to-tr from-white/40 via-orange-100/30 to-pink-100/40 backdrop-blur-lg shadow-xl rounded-3xl p-4 sm:p-8 w-full max-w-3xl mx-auto mt-6 border border-orange-200 hover:shadow-2xl transition-all duration-300"
+          className="bg-gradient-to-tr from-white/40 via-orange-100/30 to-pink-100/40 backdrop-blur-lg shadow-xl rounded-3xl p-4 sm:p-8 w-full max-w-3xl mx-auto mt-1 border border-orange-200 hover:shadow-2xl transition-all duration-300"
         >
           <div className="rounded-xl overflow-hidden mb-4 max-h-[45vh]">
             <img
               src={TeamPic}
               alt="SQAC Team"
-              className="w-full max-w-[1000px] h-auto mx-auto object-cover rounded-xl shadow-lg"
+              className=" w-full max-w-[1000px] h-auto mx-auto object-cover rounded-xl shadow-lg"
             />
           </div>
 
