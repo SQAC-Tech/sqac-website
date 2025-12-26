91466ebf7e238b7fd995d6df0cde0c842c8a61a1
sakshambajpai1604 <sakshambajpai.1604@gmail.com>
2025-12-27 02:17:38 +0530

    changed the ui

--- Frontend/src/components/JoinUs/Closed.jsx
+++ Frontend/src/components/JoinUs/Closed.jsx
@@ -11,7 +11,7 @@ const Closed = () => {
           Please check back later for future opportunities.
         </p>
         <Link to="/">
-          <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-full shadow-md transition duration-300">
+          <button className="bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 px-6 rounded-full shadow-md transition duration-300">
             Back to Home
           </button>
         </Link>
